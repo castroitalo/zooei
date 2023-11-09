d90c6c1400259df326e86a577f2e2f338c691cfe
devcastroitalo <dev.castro.italo@gmail.com>
2023-11-09 18:26:15 -0300

    zooei image added

--- src/core/DatabaseCore.php
+++ src/core/DatabaseCore.php
@@ -27,6 +27,7 @@ class DatabaseCore
      * @var array
      */
     private static array $pdoSettings = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_CASE => PDO::CASE_NATURAL
